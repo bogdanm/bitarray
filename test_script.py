66167f63e12eda4f4b370211bb2a23ea9ee0102d
Bogdan Marinescu <bogdan.marinescu@arm.com>
2016-10-26 01:23:30 +0300

    Try to fix compilation issue, part II

--- test_script.py
+++ test_script.py
@@ -8,6 +8,6 @@ for unit_size in bit_sizes:
         res = os.system("gcc -std=c99 -m32 -Os -DBA_UNIT_SIZE_BITS=%d -DBA_ELEMENT_TYPE_BITS=%d -DBA_ARRAY_SIZE_BITS=32 bitarray.c bitarray_test.c -o bitarray_test.elf" % (unit_size, element_size))
         if res != 0:
             sys.exit(1)
-        res = os.system("bitarray_test.elf")
+        res = os.system("./bitarray_test.elf")
         if res != 0:
             sys.exit(2)
